4048757a6e769e820824fbda8b43694168177ce4
alionajanusko <alionajanusko@gmail.com>
2024-07-20 12:17:47 +0100

    Create superuser for admin

--- wiki_pride/settings.py
+++ wiki_pride/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-qj8q5#_=&jf6nt)zoq3@mv6q_=c!6t0!!j#35#7th6^c8(8va^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['wikiprideapp-76ed82efb0d2.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
